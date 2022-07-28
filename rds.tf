7091183ec2212d5bce47a316270862f52ea9e9f2
Nisha Bidave <nisha.bidave@cloudreach.com>
2022-07-28 18:30:18 +0530

    few changes to rds.tf

--- rds.tf
+++ rds.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "rds_instance" {
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids =[aws_security_group.rds_server.id]
 }
 
 
